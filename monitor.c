67af53f28378e4f0c2f68c49dcf0d8c392aac7bb
Lars Hanisch <dvb@flensrocker.de>
2011-08-31 21:42:30 +0200

    use new net_register_notification function

--- monitor.c
+++ monitor.c
@@ -57,49 +57,44 @@ static char *GetFrontend(int devid)
 
 void cSundtekMonitor::Action(void)
 {
-  int fd = net_connect();
+  int fd = net_register_notification();
   if (fd < 0) {
     esyslog("sundtek: can't connect monitor to mediasrv");
     return;
   }
-  char cmd = MEDIA_CMD_NOTIFICATION;
-  if (send(fd, &cmd, 1, MSG_NOSIGNAL) < 0)
-    esyslog("sundtek: error on send: %d", errno);
-  else {
-    char buffer[1024];
-    struct pollfd pfd;
-    pfd.fd = fd;
-    pfd.events = POLLIN | POLLHUP;
-    while (true) {
-          int count = poll(&pfd, 1, 1000);
-          if (!Running())
-              break;
-          if (count == 0)
-             continue;
-          if (count < 0) {
-             esyslog("sundtek: error on poll: %d", errno);
-             continue;
-             }
-          if (pfd.revents & (POLLHUP | POLLERR | POLLNVAL)) {
-             esyslog("sundtek: error on poll: %d", errno);
-             break;
-             }
-          if (pfd.revents & POLLIN) {
-             int nread = recv(fd, buffer, 1024, MSG_DONTWAIT);
-             if (nread == sizeof(struct media_device_notification)) {
-                struct media_device_notification *mdn = (struct media_device_notification*)buffer;
-                if (mdn->status == DEVICE_ATTACHED) {
-                   char *frontend = GetFrontend(mdn->deviceid);
-                   if (frontend) {
-                      cSundtekDevice::Attach(mdn->deviceid, frontend);
-                      free(frontend);
-                      }
-                   }
-                else if (mdn->status == DEVICE_DETACHED)
-                   cSundtekDevice::Detach(mdn->deviceid);
-                }
-             }
-          }
-    }
+
+  struct pollfd pfd;
+  pfd.fd = fd;
+  pfd.events = POLLIN | POLLHUP;
+  while (true) {
+        int count = poll(&pfd, 1, 1000);
+        if (!Running())
+            break;
+        if (count == 0)
+           continue;
+        if (count < 0) {
+           esyslog("sundtek: error on poll: %d", errno);
+           continue;
+           }
+        if (pfd.revents & (POLLHUP | POLLERR | POLLNVAL)) {
+           esyslog("sundtek: error on poll: %d", errno);
+           break;
+           }
+        if (pfd.revents & POLLIN) {
+           struct media_device_notification *mdn = net_read_notification(fd);
+           if (mdn) {
+              if (mdn->status == DEVICE_ATTACHED) {
+                 char *frontend = GetFrontend(mdn->deviceid);
+                 if (frontend) {
+                    cSundtekDevice::Attach(mdn->deviceid, frontend);
+                    free(frontend);
+                    }
+                 }
+              else if (mdn->status == DEVICE_DETACHED)
+                 cSundtekDevice::Detach(mdn->deviceid);
+              free(mdn);
+              }
+           }
+        }
   net_close(fd);
 }
